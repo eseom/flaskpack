b7158b15c1ede20a519b7ea1ae24011abc300cb2
Red <red@woorooroo.com>
2018-04-14 17:13:40 +0900

    username 20 => 255

--- flaskpack/models/user.py
+++ flaskpack/models/user.py
@@ -36,7 +36,7 @@ class User(Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True)
-    username = db.Column(db.String(20), unique=True)
+    username = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
     confirmed_at = db.Column(db.DateTime())
